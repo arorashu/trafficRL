c2964c52fa384c2a9cd9d9ea823ffe12bb0b54c4
Sukriti Bisht <sukriti10august@gmail.com>
2017-05-29 12:28:49 +0530

    Modified e greedy and added options

--- runner.py
+++ runner.py
@@ -208,7 +208,7 @@ def get_options():
                          default=False, help="run the commandline version of sumo")
     optParser.add_option("--cars", "-C", dest="numberCars", default=100000, metavar="NUM",
                          help="specify the number of cars generated for simulation")
-    optParser.add_option("--bracket", dest="bracket", default=10, metavar="BRACKET",
+    optParser.add_option("--bracket", dest="bracket", default=4, metavar="BRACKET",
                          help="specify the number with which to partition the range of queue length/cumulative delay")
     optParser.add_option("--learning", dest="learn", default='1', metavar="NUM", choices= ['0', '1', '2'],
                          help="specify learning method (0 = No Learning, 1 = Q-Learning, 2 = SARSA)")
@@ -265,5 +265,5 @@ if __name__ == "__main__":
                              "-r", "data/cross.rou.xml",
                              "--queue-output", "queue.xml",
                              "--tripinfo-output", "tripinfo.xml"])
-    init()
+    init(options)
     run(options)
