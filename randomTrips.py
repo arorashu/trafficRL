7b329c14c0db62334c94b2c8fd5e25986d8a7f79
Rohit Gohri <work@rohit.page>
2021-05-03 09:23:17 +0530

    set sumo path correctly

--- randomTrips.py
+++ randomTrips.py
@@ -31,9 +31,16 @@ from collections import defaultdict
 import math
 import optparse
 
-SUMO_HOME = os.environ.get('SUMO_HOME',
-                           os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..'))
-sys.path.append(os.path.join(SUMO_HOME, 'tools'))
+# we need to import python modules from the $SUMO_HOME/tools directory
+try:
+    sys.path.append(os.path.join(os.path.dirname(
+        __file__), '..', '..', '..', '..', "tools"))  # tutorial in tests
+    sys.path.append(os.path.join(os.environ.get("SUMO_HOME", os.path.join(
+        os.path.dirname(__file__), "..", "..", "..")), "tools"))  # tutorial in docs
+    from sumolib import checkBinary
+except ImportError:
+    sys.exit(
+        "Please declare environment variable 'SUMO_HOME' as the root directory of your sumo installation (it should contain folders 'bin', 'tools' and 'docs')")
 import route2trips  # nopep8
 import sumolib  # nopep8
 
